64c04e5257baa1f69d1016e8bb7eef1d5b8d16ec
NatoGA8 <nathangallagher16@gmail.com>
2024-09-26 15:48:46 +0100

    Updated program code with if-else statements.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,10 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Type in the first number followed by the Enter key");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+﻿using System.Xml.Serialization;
 
-Console.WriteLine("Type in the second number followed by the Enter key");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+CalculatorApp();
+void CalculatorApp()
+{
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int result = 0;
+    int choice = 0;
 
-int result = firstNumber + secondNumber;
+    Console.WriteLine("Type in the first number followed by the Enter key:");
+    firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
+    Console.WriteLine("Type in the second number followed by the Enter key:");
+    secondNumber = Convert.ToInt32(Console.ReadLine());
+    //Use a loop to make a relevent decision 
+    // and perform the request maths operation.
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+
+ choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+
+        //Perform the Calculation
+        result = firstNumber + secondNumber;
+        Console.WriteLine($"Adding {firstNumber} and {secondNumber} equals {result}");
+    }
+    else if (choice == 2)
+    {
+        result = firstNumber - secondNumber;
+        Console.WriteLine($"Subtracting {firstNumber} and {secondNumber} equals: {result}");
+
+    }
+    else if (choice == 3)
+    {
+        result = firstNumber / secondNumber;
+        Console.WriteLine($"Dividing {firstNumber} and {secondNumber} equals: {result}");
+    }
+    else if (choice == 4)
+    {
+        result = firstNumber * secondNumber;
+        Console.WriteLine($"Multiplying {firstNumber} and {secondNumber} equals: {result}");
+    }
+    else
+    {
+        Console.WriteLine("You did not select a valid number between 1-4.");
+    }
+    Console.WriteLine("The result is: {0}", result);
+}
